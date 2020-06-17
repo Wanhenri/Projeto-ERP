5ccb5092f7f797c6f6ebca3b7913022d71a4b95c
Wanhenri <slipkorn.jk@gmail.com>
2020-06-17 14:08:28 -0300

    atualização do FormLeadVendor

--- app/src/component/FormLeadVendor/index.js
+++ app/src/component/FormLeadVendor/index.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import styled from 'styled-components';
 import InputData from "../../object/Inputdata";
 import Btn from "../../object/Btn";
+import {Card} from '../../object/Card'
+import { Strong } from "../../object/Strong";
 
 export const Wrapper = styled.section`
   margin-top: 85px;
@@ -14,9 +16,9 @@ export const Container = styled.section`
 
 `;
 
-export const ContainerInput = styled.section`
+export const ContainerColumn = styled.section`
   display: flex;
-  flex-flow: row wrap;
+  flex-direction: column;
 `;
 
 export const Form = styled.form``;
@@ -26,26 +28,34 @@ const FormLeadVendor = props => {
   const [search, setSearch] = useState("");
 
   return (
-    <Form>
-        <Wrapper>
-            <Container>
-                <ContainerInput>
-                    <InputData
-                      placeholder="teste"
-                    />
-                    <InputData
-                      placeholder="teste"
-                      value={search}
-                      onChange={e => setSearch(e.target.value)}
-                    />
-                </ContainerInput>
-                <Btn
-                >
-                  {loading ? "Buscando..." : "Buscar"}
-                </Btn>
-            </Container>            
-        </Wrapper>
-    </Form>
+    <Card style={{ maxWidth: 470, marginTop: 60 }}>
+      <Strong>VENDOR</Strong>
+      <Form>
+          <Wrapper>
+              <Container>
+                  <ContainerColumn>
+                      <InputData  
+                        style={{marginLeft: 47}}                    
+                        placeholder="Name"
+                      />
+                      <InputData  
+                        style={{marginTop: 3, marginLeft: 47}}                    
+                        placeholder="CNPJ"
+                      />
+                      <InputData
+                        style={{marginTop: 3, marginLeft: 47}}
+                        placeholder="City"
+                      />
+                  </ContainerColumn>
+                  <Btn
+                    style={{marginTop: 3, marginLeft: 47}}
+                  >
+                    {loading ? "Adding..." : "Add"}
+                  </Btn>
+              </Container>            
+          </Wrapper>
+      </Form>
+    </Card>
   );
 };
 
